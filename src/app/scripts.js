a563c6169555a0dfde9524dc822d0c747f5fff8f
Yadira Cisneros <yadira.cisneros@globant.com>
2018-08-29 21:44:56 -0500

    Quitar Parallax y pone el $(window).scroll

--- src/app/scripts.js
+++ src/app/scripts.js
@@ -1,5 +1,20 @@
 (function ($) {
 
+    /// Variables
+    var lastId = null,
+        $margenScroll = 30,
+        $logoInfantil = $('.ad__header-logo-infantil'),
+        $menu = $('.ad__header-menu-principal'),
+        $itemsMenu = $menu.find('a'),
+        $claseActiva = 'active',
+        $dataLogoInfantil = 'data-infantil',
+        $scrollItems = $itemsMenu.map(function(){
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+
+    /////////////////////////
+
     /// Mover la página a la sección deseada
     function scrollIt(section, offset, duration){
         var element = $(section),
@@ -10,28 +25,54 @@
 
     /// Mover la página en el click del menú
     function clickMenu(){
-        $('.ad__header-menu-principal li a').click(function(){
-            $('.ad__header-menu-principal li a').removeClass('active');
-            $(this).addClass('active');
+        $itemsMenu.click(function(e){
+            $itemsMenu.removeClass($claseActiva);
+            $(this).addClass($claseActiva);
             var anchor = $(this).attr('href');
-            if(anchor) {
-                scrollIt(anchor);
-            }
-            return false;
+            if(anchor) { scrollIt(anchor); }
+            logoInfantil($(this).attr($dataLogoInfantil));
+            e.preventDefault();
         });
     }
 
-    /// Iniciar el parallax para las vitaminas
-    function runParallax(){
-        var scene = document.getElementById('scene-vitaminas');
-        var parallaxInstance = new Parallax(scene);
+    /// Mostrar / Ocultar el logo infantil
+    function logoInfantil(infantil){
+        if(infantil){ 
+            $logoInfantil.show().prev().hide();
+        } else {
+            $logoInfantil.hide().prev().show();
+        }
     }
+
+    /// Unir el scroll para poner la clase activa 
+    $(window).scroll(function(){
+        var fromTop = $(this).scrollTop() + $margenScroll;
+        var cur = $scrollItems.map(function(){
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+        if (id && lastId !== id) {
+            lastId = id;
+            var itemActivo = $itemsMenu.filter("[href='#"+id+"']");
+            $itemsMenu.removeClass($claseActiva);
+            itemActivo.addClass($claseActiva);
+            logoInfantil(itemActivo.attr($dataLogoInfantil));
+            setTimeout(function(){
+                scrollIt('#'+id);
+            }, 500);
+            
+        }
+    });
 
     /// Iniciar
     $(document).ready(function(){
         scrollIt('body');
         clickMenu();
-        runParallax();
+        $logoInfantil.hide();
+        
     });
 
 })(jQuery);
