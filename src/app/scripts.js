e56861ac38607e96438a40d909b1f44621d708e0
Yadira Cisneros <ycisne@gmail.com>
2018-10-04 02:27:30 -0500

    Optimización de tamaño de imágenes; Cambios a la fuente para usar Roboto; Banner de cookies y creación de cookie; Cambios hechos por cliente en textos de Legales; Brillos atrás y quitar botón de Instagram

--- src/app/scripts.js
+++ src/app/scripts.js
@@ -4,6 +4,16 @@
     var lastId = null,
         $margenScroll = 30,
         $claseActiva = 'active',
+        
+        /// Cookies
+        $header = $('.ad__header'),
+        $addCookies = 'ad__has-cookies',
+        $removeCookies = 'ad__no-cookies',
+        $firstScroll = true,
+        $cookie = 'aceptarAderogyl',
+        $cookieValue = 'true',
+        $addCookiesBtn = $('.ad__cookies-button a'),
+        $noCookie = true,
 
         /// Menú principal
         $menu = $('.ad__header-menu-principal'),
@@ -91,6 +101,9 @@
                     case 'closeVideo':
                         closeModal(anchor, true);
                         break;
+                    case 'addCookie':
+                        removeCookieBanner();
+                        addCookie();
                 }
             }
             e.preventDefault();
@@ -101,8 +114,9 @@
     function closeModal(anchor, isVideo){
         $(anchor).animate({
             opacity: 0
-        }, 100, function(){
-            $('body').removeClass('overflowHidden');
+        }, 500, function(){
+            restoreWheel();
+            //$('body').removeClass('overflowHidden');
         });
         setTimeout(function(){
             $(anchor).removeAttr('style');
@@ -116,7 +130,7 @@
     function closeModalAndShow(anchor, show){
         $(show)
             .css({visibility: 'visible'})
-            .animate({opacity: 1}, 100);
+            .animate({opacity: 1}, 500);
         closeModal(anchor);
     }
 
@@ -170,18 +184,20 @@
 
     /// Mostar modal
     function showModal(anchor, hide, isVideo){
-        $('body').addClass('overflowHidden');
+        preventWheel();
+        //$('body').addClass('overflowHidden');
         $(anchor).css({
             visibility: 'visible',
             display: 'flex'
-        }).animate({ opacity: 1}, 100);
+        }).animate({ opacity: 1}, 500);
         bindKeyEvent(anchor, hide, isVideo);
     }
 
     /// Mostrar u ocultar presentación
     function showHidePresentacion(anchor, clase){
         if(clase) {
-            $('body').addClass('overflowHidden');
+            preventWheel();
+            //$('body').addClass('overflowHidden');
             $(anchor).removeClass().addClass(clase);
             $(document).on('keydown', function(e){
                 var code = e.keyCode || e.which;
@@ -194,8 +210,74 @@
             $(anchor).addClass('ad__transition');
             setTimeout(function(){
                 $(anchor).removeClass();
-                $('body').removeClass('overflowHidden');
+                restoreWheel();
+                //$('body').removeClass('overflowHidden');
+
             }, 1000);
+        }
+    }
+
+    /// Prevenir qu se mueva el contenido al girar la rueda del mouse
+    function preventWheel(){
+        document.onmousewheel = function(){ stopWheel(); }; /* IE7, IE8 */
+        if(document.addEventListener){ /* Chrome, Safari, Firefox */
+            document.addEventListener('DOMMouseScroll', stopWheel, false);
+        }
+    }
+
+    function stopWheel(e){
+        if(!e){ e = window.event; } /* IE7, IE8, Chrome, Safari */
+        if(e.preventDefault) { e.preventDefault(); } /* Chrome, Safari, Firefox */
+        e.returnValue = false; /* IE7, IE8 */
+    }
+
+
+    // Restaura el movimiento del mouse
+    function restoreWheel(){
+        document.onmousewheel = null;  /* IE7, IE8 */
+        if(document.addEventListener){ /* Chrome, Safari, Firefox */
+            document.removeEventListener('DOMMouseScroll', stopWheel, false);
+        }
+    }
+
+    /// Quitar banner de cookie
+    function removeCookieBanner(){
+        $header.removeClass($addCookies).addClass($removeCookies);
+        setTimeout(function(){
+            $header.removeClass($removeCookies);
+            $firstScroll = false;
+        }, 1200);
+    }
+
+    /// Agregar la Cookie
+    function addCookie(){
+        document.cookie = $cookie + '=' + $cookieValue;
+        $noCookie = false;
+    }
+    
+    /// Leer la cookie
+    function readCookie(){
+        var cookies = document.cookie.split(';');
+        if(cookies.length > 0){
+            cookies.map(function(item){
+                var splitCookie = item.split('=');
+                if(splitCookie[0] == $cookie){
+                    if(splitCookie[1] != $cookieValue){
+                        console.log('No existe la cookie, mostrar banner');
+                        $header.addClass($addCookies);
+                    } else {
+                        console.log('La cookie ya existe, no mostrar banner');
+                        $noCookie = false;
+                    }
+                } else {
+                    console.log('No existe la cookie, mostrar banner');
+                    $header.addClass($addCookies);
+                }
+                return item;
+            });
+        } else {
+            console.log('No existe la cookie, mostrar banner');
+            $header.addClass($addCookies);
         }
     }
 
@@ -203,10 +285,24 @@
     $(window).scroll(function(){
         var scrollTop = $(this).scrollTop(),
             fromTop = scrollTop + $margenScroll,
+            heightWindow = ($(this).height()) / 2,
             cur = $scrollItems.map(function(){
                 if ($(this).offset().top < fromTop)
                     return this;
             });
+        
+        /// Si el scroll llega a más de la mitad del alto de la ventana, 
+        /// quitar banner de cookie si la cookie no existe
+        if($noCookie){
+            if(scrollTop > heightWindow && $firstScroll){
+                removeCookieBanner();
+            } else {
+                if(scrollTop == 0){
+                    $header.addClass($addCookies);
+                    $firstScroll = true;
+                }
+            }
+        }
         
         cur = cur[cur.length-1];
         var id = cur && cur.length ? cur[0].id : "";
@@ -225,6 +321,7 @@
 
     /// Iniciar
     $(document).ready(function(){
+        readCookie();
         scrollIt('body');
         clickMenu($itemsMenu, 'menu');
         clickMenu($itemsPreguntas, 'faq');
@@ -235,7 +332,9 @@
         clickMenu($cerrarPresentacion, 'hidePresentacion');
         openVideo($videoBtn);
         clickMenu($videoBtnClose, 'closeVideo');
+        clickMenu($addCookiesBtn, 'addCookie');
         $logoInfantil.hide();
+        // document.cookie = 'aceptarAderogyl=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
     });
 
 })(jQuery);
